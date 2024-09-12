57d955559b001425151aa0a2c1375079835b01cc
leehosung1007 <dodospy12@naver.com>
2024-09-12 16:29:55 +0900

    2주차 실습파일

---
+++ 2week/ex0.py
@@ -0,0 +1,10 @@
+num1 = int(input("값을 입력하세요: "))
+num2 = num1 % 2
+
+result = (num2 % 2) == 0
+print("짝수이다 -", result)
+
+#if num2 == 0:
+#    print("입력한 값이 짝수입니다.")   
+#if num2 != 0:
+#    print("입력한 값이 홀수입니다.")
